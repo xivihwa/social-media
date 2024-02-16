53f753a9eb4e9480517d85ac16c94525fb3e53cc
Violetta Antonenko <antonenko.violetta@lll.kpi.ua>
2024-02-16 17:33:53 +0200

    routes posts/users

--- server/index.js
+++ server/index.js
@@ -10,7 +10,11 @@ import { fileURLToPath } from "url";
 import multer from "multer";
 import { error } from "console";
 import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js";
+import { verifyToken } from "./middleware/auth.js";
 
 // CONFIGURATIONS
 const __filename = fileURLToPath(import.meta.url);
@@ -39,9 +43,12 @@ const upload = multer({ storage });
 
 // ROUTES WITH FILES
 app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 // ROUTES
 app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
 // MONGOOSE SETUP
 const PORT = process.env.PORT || 6001;
