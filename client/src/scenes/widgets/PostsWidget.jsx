00607430ed1028870811e093fa34311f2c8f4eec
Violetta <antonenko.violetta@lll.kpi.ua>
2024-06-07 01:04:07 +0300

    another fix

--- client/src/scenes/widgets/PostsWidget.jsx
+++ client/src/scenes/widgets/PostsWidget.jsx
@@ -63,8 +63,8 @@ const PostsWidget = ({ userId, isProfile = false }) => {
             name={`${firstName} ${lastName}`}
             description={description}
             location={location}
-            picturePath={files.find(elem => elem.fileType === "picture").filePath | picturePath}
-            videoPath={files.find(elem => elem.fileType === "video").filePath}
+            picturePath={picturePath | files.find(elem => elem.fileType === "picture").filePath}
+            videoPath={videoPath | files.find(elem => elem.fileType === "video").filePath}
             attachmentPath={attachmentPath}
             audioPath={audioPath}
             userPicturePath={userPicturePath}
